46268a74107d2f00a5e89e3b10bab937053298df
Alexander Semyonov <al@semyonov.us>
2014-08-04 03:28:38 +0400

    Use Active Admin for management

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803231839) do
+ActiveRecord::Schema.define(version: 20140803232234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], :name => "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], :name => "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
