90715b76f08c99f2f9ca1db7008209d466f9355c
NetBUG <darikcr@gmail.com>
2014-11-06 18:21:53 +0300

    Fixed compatibility issue

--- db/migrate/20141031030802_migrate_db_format.rb
+++ db/migrate/20141031030802_migrate_db_format.rb
@@ -37,7 +37,7 @@ class MigrateDbFormat < ActiveRecord::Migration
 
   def change
     puts "Installing dependencies...\n"
-    cmd = "sudo apt-get install ruby-bson ruby-bson-ext ruby-mongo; " + I18n.t('mongoupdate')
+    cmd = "sudo gem instal mongoid; " + I18n.t('mongoupdate')
     res = %x( #{cmd} )
     Dir.glob(Rails.root.to_s + "/config/locales/??.yml").each do |file| 
 		cmd = "cat " + file + " | grep -v mongo > " + file + "_; mv " + file + "_ " + file
